5aa4372829d6e9f5453bf38dfe389cb561cb3e20
Quentin Campbell <quentin@maxheadroom.co.za>
2014-05-16 12:13:01 +0200

    Added bootstrap twocol onepage layout

---
+++ modules/custom/di2d_panels_bootstrap/plugins/layouts/bootstrap_twocol_onepage/bootstrap-twocol-onepage.tpl.php
@@ -0,0 +1,35 @@
+<div class="<?php print $classes ?>" <?php if (!empty($css_id)) {
+  print "id=\"$css_id\"";
+} ?>>
+  <div class="row">
+    <?php print $content['row1']; ?>
+  </div>
+  <div class="row">
+<?php print $content['left1']; ?>
+    <?php print $content['right1']; ?>
+  </div>
+  <div class="row">
+    <?php print $content['row2']; ?>
+  </div>
+  <div class="row">
+<?php print $content['left2']; ?>
+    <?php print $content['right2']; ?>
+  </div>
+  <div class="row">
+    <?php print $content['row3']; ?>
+  </div>
+  <div class="row">
+<?php print $content['left3']; ?>
+    <?php print $content['right3']; ?>
+  </div>
+  <div class="row">
+    <?php print $content['row4']; ?>
+  </div>
+  <div class="row">
+<?php print $content['left4']; ?>
+    <?php print $content['right4']; ?>
+  </div>
+  <div class="row">
+<?php print $content['row5']; ?>
+  </div>
+</div>
